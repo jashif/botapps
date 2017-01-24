3ec11b26279988c985724a78ad9fbf0718a9f9b8
Aboobacker <jashif.aboobacker@capgemini.com>
2017-01-24 15:52:29 +0530

    First changed

--- CSharp/intelligence-LUIS/Dialogs/RootLuisDialog.cs
+++ CSharp/intelligence-LUIS/Dialogs/RootLuisDialog.cs
@@ -1,4 +1,4 @@
-﻿#define useSampleModel
+﻿//#define useSampleModel
 
 namespace LuisBot.Dialogs
 {
@@ -12,206 +12,271 @@ namespace LuisBot.Dialogs
     using Microsoft.Bot.Builder.Luis;
     using Microsoft.Bot.Builder.Luis.Models;
     using Microsoft.Bot.Connector;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using Newtonsoft.Json;
 
     [Serializable]
+
 #if useSampleModel
+
     [LuisModel("162bf6ee-379b-4ce4-a519-5f5af90086b5", "11be6373fca44ded80fbe2afa8597c18")]
+
 #else
-    [LuisModel("YourModelId", "YourSubscriptionKey")]
+
+    [LuisModel("b7ee19ae-b6b5-47c1-8a52-f567bbd6be29", "d9228b45e7d54b2c98dd173f002f83c4")]
+
 #endif
+
+
+
     public class RootLuisDialog : LuisDialog<object>
+
     {
-        private const string EntityGeographyCity = "builtin.geography.city";
 
-        private const string EntityHotelName = "Hotel";
+        [LuisIntent("GetExpense")]
 
-        private const string EntityAirportCode = "AirportCode";
+        public async Task GetExpense(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
 
-        private IList<string> titleOptions = new List<string> { "“Very stylish, great stay, great staff”", "“good hotel awful meals”", "“Need more attention to little things”", "“Lovely small hotel ideally situated to explore the area.”", "“Positive surprise”", "“Beautiful suite and resort”" };
+        {
+
+
+
+            var message = await activity;
+
+            await context.PostAsync($"Welcome to the expense buddy support! we are analyzing your message: '{message.Text}'...");
+
+
+
+            var hotelsQuery = new ExpenseQuery();
+
+
+
+            var hotelsFormDialog = new FormDialog<ExpenseQuery>(hotelsQuery, this.BuildHotelsForm, FormOptions.PromptInStart, result.Entities);
+
+
+
+            context.Call(hotelsFormDialog, this.ResumeAfterHotelsFormDialog);
+
+        }
 
         [LuisIntent("")]
+
         [LuisIntent("None")]
+
         public async Task None(IDialogContext context, LuisResult result)
+
         {
+
             string message = $"Sorry, I did not understand '{result.Query}'. Type 'help' if you need assistance.";
+
+
 
             await context.PostAsync(message);
 
-            context.Wait(this.MessageReceived);
-        }
 
-        [LuisIntent("SearchHotels")]
-        public async Task Search(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
-        {
-            var message = await activity;
-            await context.PostAsync($"Welcome to the Hotels finder! We are analyzing your message: '{message.Text}'...");
-
-            var hotelsQuery = new HotelsQuery();
-
-            EntityRecommendation cityEntityRecommendation;
-
-            if (result.TryFindEntity(EntityGeographyCity, out cityEntityRecommendation))
-            {
-                cityEntityRecommendation.Type = "Destination";
-            }
-
-            var hotelsFormDialog = new FormDialog<HotelsQuery>(hotelsQuery, this.BuildHotelsForm, FormOptions.PromptInStart, result.Entities);
-
-            context.Call(hotelsFormDialog, this.ResumeAfterHotelsFormDialog);
-        }
-
-        [LuisIntent("ShowHotelsReviews")]
-        public async Task Reviews(IDialogContext context, LuisResult result)
-        {
-            EntityRecommendation hotelEntityRecommendation;
-
-            if (result.TryFindEntity(EntityHotelName, out hotelEntityRecommendation))
-            {
-                await context.PostAsync($"Looking for reviews of '{hotelEntityRecommendation.Entity}'...");
-
-                var resultMessage = context.MakeMessage();
-                resultMessage.AttachmentLayout = AttachmentLayoutTypes.Carousel;
-                resultMessage.Attachments = new List<Attachment>();
-
-                for (int i = 0; i < 5; i++)
-                {
-                    var random = new Random(i);
-                    ThumbnailCard thumbnailCard = new ThumbnailCard()
-                    {
-                        Title = this.titleOptions[random.Next(0, this.titleOptions.Count - 1)],
-                        Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris odio magna, sodales vel ligula sit amet, vulputate vehicula velit. Nulla quis consectetur neque, sed commodo metus.",
-                        Images = new List<CardImage>()
-                        {
-                            new CardImage() { Url = "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif" }
-                        },
-                    };
-
-                    resultMessage.Attachments.Add(thumbnailCard.ToAttachment());
-                }
-
-                await context.PostAsync(resultMessage);
-            }
 
             context.Wait(this.MessageReceived);
+
         }
 
         [LuisIntent("Help")]
+
         public async Task Help(IDialogContext context, LuisResult result)
+
         {
-            await context.PostAsync("Hi! Try asking me things like 'search hotels in Seattle', 'search hotels near LAX airport' or 'show me the reviews of The Bot Resort'");
+
+            await context.PostAsync("Hi! please type like 'find my expense','get my expense' etc");
+
+
 
             context.Wait(this.MessageReceived);
+
         }
 
-        private IForm<HotelsQuery> BuildHotelsForm()
+
+
+        private async Task ResumeAfterHotelsFormDialog(IDialogContext context, IAwaitable<ExpenseQuery> result)
+
         {
-            OnCompletionAsyncDelegate<HotelsQuery> processHotelsSearch = async (context, state) =>
+
+            try
+
             {
-                var message = "Searching for hotels";
-                if (!string.IsNullOrEmpty(state.Destination))
+
+                var searchQuery = await result;
+
+
+
+                var hotels = await this.GetExpenseByEmailId(searchQuery.EmailId);
+
+
+
+                await context.PostAsync($"I found your expense");
+
+
+
+                var resultMessage = context.MakeMessage();
+
+                resultMessage.Text = "your expense for current month  is Rs: " + hotels.amount;
+
+
+
+
+
+
+
+                await context.PostAsync(resultMessage);
+
+            }
+
+            catch (FormCanceledException ex)
+
+            {
+
+                string reply;
+
+
+
+                if (ex.InnerException == null)
+
                 {
-                    message += $" in {state.Destination}...";
+
+                    reply = "You have canceled the operation.";
+
                 }
-                else if (!string.IsNullOrEmpty(state.AirportCode))
+
+                else
+
                 {
-                    message += $" near {state.AirportCode.ToUpperInvariant()} airport...";
+
+                    reply = $"Oops! Something went wrong :( Technical Details: {ex.InnerException.Message}";
+
+                }
+
+
+
+                await context.PostAsync(reply);
+
+            }
+
+            finally
+
+            {
+
+                context.Done<object>(null);
+
+            }
+
+        }
+
+        public async Task<ExpenseData> GetExpenseByEmailId(string userid)
+
+        {
+
+            HttpClient client = new HttpClient();
+
+            // client.BaseAddress = new Uri("http://expensebuddy.azurewebsites.net/api/botdata/myexpense?emailId=" + userid);
+
+            client.DefaultRequestHeaders.Accept.Add(
+
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // string parameter = string.Format(Consts.LookupBikesWithUserAPI, userid);
+
+            HttpResponseMessage response = client.GetAsync("http://expensebuddy.azurewebsites.net/api/botdata/myexpense?emailId=" + userid).Result;
+
+            if (response.IsSuccessStatusCode)
+
+            {
+
+                JsonSerializerSettings settings = new JsonSerializerSettings();
+
+                String responseString = await response.Content.ReadAsStringAsync();
+
+                var responseElement = JsonConvert.DeserializeObject<ExpenseData>(responseString, settings);
+
+                return responseElement;
+
+            }
+
+            else
+
+            {
+
+                return null;
+
+            }
+
+        }
+
+        private IForm<ExpenseQuery> BuildHotelsForm()
+
+        {
+
+            OnCompletionAsyncDelegate<ExpenseQuery> processHotelsSearch = async (context, state) =>
+
+            {
+
+                var message = "Getting  expense of ";
+
+                if (!string.IsNullOrEmpty(state.EmailId))
+
+                {
+
+                    message += $"in {state.EmailId}...";
+
                 }
 
                 await context.PostAsync(message);
+
             };
 
-            return new FormBuilder<HotelsQuery>()
-                .Field(nameof(HotelsQuery.Destination), (state) => string.IsNullOrEmpty(state.AirportCode))
-                .Field(nameof(HotelsQuery.AirportCode), (state) => string.IsNullOrEmpty(state.Destination))
+
+
+            return new FormBuilder<ExpenseQuery>()
+
+                .Field(nameof(ExpenseQuery.EmailId), (state) => string.IsNullOrEmpty(state.EmailId))
+
+
+
                 .OnCompletion(processHotelsSearch)
+
                 .Build();
+
         }
 
-        private async Task ResumeAfterHotelsFormDialog(IDialogContext context, IAwaitable<HotelsQuery> result)
-        {
-            try
-            {
-                var searchQuery = await result;
 
-                var hotels = await this.GetHotelsAsync(searchQuery);
 
-                await context.PostAsync($"I found {hotels.Count()} hotels:");
 
-                var resultMessage = context.MakeMessage();
-                resultMessage.AttachmentLayout = AttachmentLayoutTypes.Carousel;
-                resultMessage.Attachments = new List<Attachment>();
 
-                foreach (var hotel in hotels)
-                {
-                    HeroCard heroCard = new HeroCard()
-                    {
-                        Title = hotel.Name,
-                        Subtitle = $"{hotel.Rating} starts. {hotel.NumberOfReviews} reviews. From ${hotel.PriceStarting} per night.",
-                        Images = new List<CardImage>()
-                        {
-                            new CardImage() { Url = hotel.Image }
-                        },
-                        Buttons = new List<CardAction>()
-                        {
-                            new CardAction()
-                            {
-                                Title = "More details",
-                                Type = ActionTypes.OpenUrl,
-                                Value = $"https://www.bing.com/search?q=hotels+in+" + HttpUtility.UrlEncode(hotel.Location)
-                            }
-                        }
-                    };
+    }
 
-                    resultMessage.Attachments.Add(heroCard.ToAttachment());
-                }
+    [Serializable]
 
-                await context.PostAsync(resultMessage);
-            }
-            catch (FormCanceledException ex)
-            {
-                string reply;
+    public class ExpenseQuery
 
-                if (ex.InnerException == null)
-                {
-                    reply = "You have canceled the operation.";
-                }
-                else
-                {
-                    reply = $"Oops! Something went wrong :( Technical Details: {ex.InnerException.Message}";
-                }
+    {
 
-                await context.PostAsync(reply);
-            }
-            finally
-            {
-                context.Done<object>(null);
-            }
-        }
+        [Prompt("Please enter your {&}")]
 
-        private async Task<IEnumerable<Hotel>> GetHotelsAsync(HotelsQuery searchQuery)
-        {
-            var hotels = new List<Hotel>();
+        [Optional]
 
-            // Filling the hotels results manually just for demo purposes
-            for (int i = 1; i <= 5; i++)
-            {
-                var random = new Random(i);
-                Hotel hotel = new Hotel()
-                {
-                    Name = $"{searchQuery.Destination ?? searchQuery.AirportCode} Hotel {i}",
-                    Location = searchQuery.Destination ?? searchQuery.AirportCode,
-                    Rating = random.Next(1, 5),
-                    NumberOfReviews = random.Next(0, 5000),
-                    PriceStarting = random.Next(80, 450),
-                    Image = $"https://placeholdit.imgix.net/~text?txtsize=35&txt=Hotel+{i}&w=500&h=260"
-                };
+        public string EmailId { get; set; }
 
-                hotels.Add(hotel);
-            }
+    }
 
-            hotels.Sort((h1, h2) => h1.PriceStarting.CompareTo(h2.PriceStarting));
+    [Serializable]
 
-            return hotels;
-        }
+    public class ExpenseData
+
+    {
+
+        public double amount { get; set; }
+
+        public bool status { get; set; }
+
+        //{"amount":2908.0,"status":true}
+
     }
 }
